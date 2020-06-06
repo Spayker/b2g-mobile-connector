6b5e34c26f66aae48ac1a2b4b4a06d4f5a898528
spayker <spykerstar@gmail.com>
2020-06-06 23:56:47 +0200

    B2GM-6 final update of device screen

--- src/components/mainMenu/tab/account/account.js
+++ src/components/mainMenu/tab/account/account.js
@@ -1,19 +1,68 @@
 import React from 'react'
 import {View, Image, Text} from 'react-native';
-import styles from "./styles.js";
+import FastImage from 'react-native-fast-image'
+import ReactNativeSettingsPage, { NavigateRow } from 'react-native-settings-page';
+import styles from './styles';
 
 export default class Account extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-        
+            name: "",
+            email: "",
+            picture: null
         }
     }
 
     render() {
         return (
-            <View style={styles.container}/>
+            <ReactNativeSettingsPage>
+
+                <View style={styles.container}>
+                    
+                    {this.state.picture === null ? (   
+                        <Image style={styles.image} source={require('../../../../resources/user_pick.png')} />    
+                    ) : (
+                        <FastImage style={styles.image}
+                            source={{
+                                uri: this.state.picture, 
+                                priority: FastImage.priority.high,
+                            }}
+                            resizeMode={FastImage.resizeMode.contain} 
+                        />
+                    )}
+                    
+                    <Text style={styles.contentTextHeader}>username</Text>
+                    <Text style={styles.contentTextDescription}>user mailbox</Text>
+                </View>
+
+                <NavigateRow style={styles.contentTextHeader}
+                    text='Height'
+                    iconName='arrows-v'
+                    onPressCallback={() => {console.log()}} />
+
+                <NavigateRow style={styles.navigationRow}
+                    text='Weight'
+                    iconName='cubes'
+                    onPressCallback={() => {console.log()}} />
+                
+                <NavigateRow style={styles.navigationRow}
+                    text='Gender'
+                    iconName='intersex'
+                    onPressCallback={() => {console.log()}} />
+
+                <NavigateRow style={styles.navigationRow}
+                    text='Age'
+                    iconName='calendar'
+                    onPressCallback={() => {console.log()}} />
+                
+                <NavigateRow style={styles.navigationRow}
+                    text='Log out'
+                    iconName='sign-out'
+                    onPressCallback={() => {console.log()}} />
+
+            </ReactNativeSettingsPage>
         );
     }
 
--- src/components/mainMenu/tab/account/styles.js
+++ src/components/mainMenu/tab/account/styles.js
@@ -4,23 +4,30 @@ export default styles = StyleSheet.create({
 
     container:{
         flex: 1,
-        justifyContent: "center",
-        backgroundColor: "white",
+        alignItems: "center",
         padding: 15,
-        paddingBottom: 75
+    },
+
+    contentTextHeader:{
+        textAlign: "center",
+        color: "#9e9e9e",
+        fontSize: 20,
+        marginTop: 10
     },
 
     contentTextDescription:{
         textAlign: "center",
-        marginTop: 16,
+        marginTop: 8,
         color: "#9e9e9e",
         fontSize: 16
     },
 
     image:{
         alignSelf: "center",
-        marginTop: 24,
-        marginBottom: 50
+        marginTop: 64,
+        marginBottom: 16,
+        width: 100,
+        height: 100
     }
 
 });
