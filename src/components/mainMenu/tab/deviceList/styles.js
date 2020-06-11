75fb60ac718e4e6fd39b793ef66536c86f55324d
spayker <spykerstar@gmail.com>
2020-06-11 21:41:38 +0200

    B2GM-9 updated styles

--- src/components/mainMenu/tab/deviceList/styles.js
+++ src/components/mainMenu/tab/deviceList/styles.js
@@ -5,7 +5,7 @@ export default styles = StyleSheet.create({
     container:{
         flex: 1,
         justifyContent: "center",
-        backgroundColor: "#333333",
+        backgroundColor: "#060403",
         padding: 15,
     },
 
@@ -18,8 +18,6 @@ export default styles = StyleSheet.create({
 
     image:{
         alignSelf: "center",
-        marginTop: 24,
-        marginBottom: 50
     },
 
     loginButton: {
@@ -37,6 +35,7 @@ export default styles = StyleSheet.create({
     },
 
     itemButtonEnabled: {
+        width: 75,
         height: 35,
         borderRadius: 30,
         backgroundColor: "#158A15",
@@ -44,7 +43,6 @@ export default styles = StyleSheet.create({
         shadowOpacity: 0.8,
         elevation: 6,
         shadowRadius: 15 ,
-        shadowOffset : { width: 1, height: 5},
         alignItems: 'center'
     },
 
@@ -56,7 +54,6 @@ export default styles = StyleSheet.create({
         shadowOpacity: 0.8,
         elevation: 6,
         shadowRadius: 15 ,
-        shadowOffset : { width: 1, height: 5},
         alignItems: 'center'
     },
 
@@ -64,8 +61,7 @@ export default styles = StyleSheet.create({
         color: "#FFFFFF",
         height: 65,
         fontSize: 16,
-        paddingTop: 20,
-        alignItems: 'center'
+        paddingTop: 5
     },
 
     listTrainingContainer:{
@@ -73,20 +69,19 @@ export default styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: 'center',
         justifyContent: "space-between",
+        backgroundColor: '#333333',
+        borderRadius: 5,
+        padding: 15
     },
 
     listTrainingColumnData:{
         alignSelf: "center",
         flexDirection: "column",
-        justifyContent: "space-between",
     },
 
-
     item: {
-        padding: 10,
         fontSize: 18,
-        height: 44,
         color: "white",
-      }
+    }
 
 });
