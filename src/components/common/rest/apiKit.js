05d8e4db90e0c2607b2dbb34f2bbf641bcba2674
spayker <spykerstar@gmail.com>
2021-12-07 19:14:10 +0100

    init commit

--- src/components/common/rest/apiKit.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import globals from '../globals'
-
-// Create axios client, pre-configured with baseURL
-let APIKit = axios.create({
-  timeout: 10000,
-});
-
-export default APIKit
