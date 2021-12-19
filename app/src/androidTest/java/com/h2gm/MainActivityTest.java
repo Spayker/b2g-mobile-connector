90fb3d8bb987c5fc3c5489f7a9daa6154f6a1c37
spayker <spykerstar@gmail.com>
2021-12-19 19:55:33 +0100

    added espresso test suit adapted for androidX

---
+++ app/src/androidTest/java/com/h2gm/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.h2gm;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> loginActivityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void click_Back_Button_Shows_MainActivity(){
+
+    }
+
+
+
+}
