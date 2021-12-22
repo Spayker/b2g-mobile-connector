7182231bc2671fb3ecb381726a1b2716e259685a
spayker <spykerstar@gmail.com>
2021-12-22 22:12:56 +0100

    added sign in activity sources

---
+++ app/src/androidTest/java/com/h2gm/activity/auth/SignInActivityTest.java
@@ -0,0 +1,24 @@
+package com.h2gm.activity.auth;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
+public class SignInActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<SignInActivity> loginActivityRule =
+            new ActivityScenarioRule<>(SignInActivity.class);
+
+    @Test
+    public void click_Back_Button_Shows_LoginActivity(){
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/h2gm/activity/auth/SignInActivity.java
@@ -0,0 +1,100 @@
+package com.h2gm.activity.auth;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+
+import com.h2gm.R;
+import com.h2gm.sensor.SensorDataGrabber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * A login screen that offers login via email/password.
+ *
+ * @author  Spayker
+ * @version 1.0
+ * @since   12/22/2021
+ */
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher,
+        CompoundButton.OnCheckedChangeListener {
+
+
+
+    /**
+     * Perform initialization of all fragments of current activity.
+     *
+     * @param savedInstanceState an instance of Bundle instance
+     *                           (A mapping from String keys to various Parcelable values)
+     **/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        setTitle(R.string.signin_title_activity);
+
+        // Views
+        // TextView mForgotPasswordView = findViewById(R.id.forgot_password);
+        // mForgotPasswordView.setPaintFlags(mForgotPasswordView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        // Buttons
+        // findViewById(R.id.email_sign_in_button).setOnClickListener(this);
+
+        //// business logic init
+
+        // sensor API
+        final SensorDataGrabber sensorDataGrabber = new SensorDataGrabber(getApplicationContext());
+        sensorDataGrabber.displayAvailableSensors();
+
+    }
+
+    /**
+     * Called after onCreate(Bundle) — or after onRestart() when the activity had been stopped,
+     * but is now again being displayed to the user. It will be followed by onResume().
+     **/
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    /**
+     * A callback method to be invoked when a view is clicked.
+     *
+     * @param view an instance of View class
+     *             ( class represents the basic building block for user interface components )
+     **/
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+}
