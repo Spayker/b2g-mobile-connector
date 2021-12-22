f991b0811f62ceadfc6fc9878a69839c334d45bc
spayker <spykerstar@gmail.com>
2021-12-22 21:57:47 +0100

    added sensor data grabber

--- app/src/main/java/com/h2gm/activity/MainActivity.java
+++ app/src/main/java/com/h2gm/activity/MainActivity.java
@@ -1,8 +1,11 @@
-package com.h2gm;
+package com.h2gm.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.h2gm.R;
+import com.h2gm.sensor.SensorDataGrabber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //// business logic init
+
+        // sensor API
+        final SensorDataGrabber sensorDataGrabber = new SensorDataGrabber(getApplicationContext());
+        sensorDataGrabber.displayAvailableSensors();
     }
 
 }
